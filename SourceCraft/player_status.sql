0e8495e58e87598e3f2e70207e6c14cc7f873de9
Naris <Naris@c696e740-cb52-2f4d-bffe-fa7f5725c3b6>
2008-03-23 02:21:57 +0000

    git-svn-id: https://naris/svn/Jiggly/trunk@815 c696e740-cb52-2f4d-bffe-fa7f5725c3b6

--- SourceCraft/player_status.sql
+++ SourceCraft/player_status.sql
@@ -1,8 +1,8 @@
 SELECT    p.player_ident, name, overall_level,
-          r.long_name, pr.xp, pr.level, ps.skill, ps.skill_level
+          r.long_name, pr.xp, pr.level, ps.upgrade, ps.upgrade_level
      FROM sc_players p
-LEFT JOIN sc_player_races pr  ON p.player_ident = pr.player_ident
-LEFT JOIN sc_player_skills ps ON p.player_ident = ps.player_ident
-                             AND pr.race_ident = ps.race_ident
-LEFT JOIN sc_races   r        ON pr.race_ident = r.race_ident
-ORDER BY p.player_ident, r.race_ident, skill
+LEFT JOIN sc_player_races pr    ON p.player_ident = pr.player_ident
+LEFT JOIN sc_player_upgrades ps ON p.player_ident = ps.player_ident
+                               AND pr.race_ident = ps.race_ident
+LEFT JOIN sc_races r            ON pr.race_ident = r.race_ident
+ORDER BY p.player_ident, r.race_ident, upgrade
